95d6f52194dddadafd31539a25fa06b3c577288a
Jeremy Yallop <yallop@gmail.com>
2016-11-28 17:11:14 +0000

    Replace remaining String.iter calls with Memcpy.

--- lib/profuse_7_23.ml
+++ lib/profuse_7_23.ml
@@ -1081,24 +1081,28 @@ module Out = struct
           loop acc ~read ~emit:(off = offset) l
       in loop [] ~read:0 ~emit:false listing      
 
+    let store ~ino ~off ~name ~typ dirent =
+      let p = coerce (ptr T.t) (ptr char) (addr dirent) in
+      begin
+        setf dirent T.ino     (UInt64.of_int64 ino);
+        setf dirent T.off     (UInt64.of_int off);
+        setf dirent T.namelen (UInt32.of_int (String.length name));
+        setf dirent T.typ     (UInt32.of_int (int_of_char typ));
+        Memcpy.(unsafe_memcpy ocaml_bytes pointer)
+          ~src:(Bytes.of_string name) ~src_off:0
+          ~dst:p ~dst_off:hdrsz ~len:(String.length name)
+      end
+
     let of_list ~host listing offset read_size req =
       let phost = host.Host.dirent.Dirent.Host.file_kind in
       let listing, count = filter_listing listing offset read_size in
       let pkt = Hdr.packet ~count req in
       let buf = CArray.start pkt in
       let ep = List.fold_left (fun p (off,ino,name,typ) ->
-        let sz = size name in
-        let dirent = !@ (coerce (ptr char) (ptr T.t) p) in
-        let typ = Dirent.File_kind.(to_code ~host:phost typ) in
-        setf dirent T.ino     (UInt64.of_int64 ino);
-        setf dirent T.off     (UInt64.of_int off);
-        setf dirent T.namelen (UInt32.of_int (String.length name));
-        setf dirent T.typ     (UInt32.of_int (int_of_char typ));
-        let sp = ref (p +@ hdrsz) in
-        (* TODO: better copy *)
-        String.iter (fun c -> !sp <-@ c; sp := !sp +@ 1) name;
-        (* Printf.eprintf "dirent serialized %s\n%!" name; *)
-        p +@ sz
+          let sz = size name in
+          let typ = Dirent.File_kind.(to_code ~host:phost typ) in
+          store ~ino ~off ~name ~typ !@(coerce (ptr char) (ptr T.t) p);
+          p +@ sz
       ) buf (List.rev listing) in
       assert (ptr_diff buf ep = count);
       pkt
@@ -1133,9 +1137,10 @@ module Out = struct
     let create ~target req =
       let count = String.length target in
       let pkt = Hdr.packet ~count req in
-      let sp = ref (CArray.start pkt) in
-      (* TODO: FIXME should not iterate! *)
-      String.iter (fun c -> !sp <-@ c; sp := !sp +@ 1) target;
+      let sp = CArray.start pkt in
+      Memcpy.(unsafe_memcpy ocaml_bytes pointer)
+        ~src:(Bytes.of_string target) ~src_off:0
+        ~dst:sp ~dst_off:0 ~len:count;
       pkt
   end
 
